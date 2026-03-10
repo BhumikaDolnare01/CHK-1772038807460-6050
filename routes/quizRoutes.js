dedfb1db2ab63aee8e03e28e21e3c159bed66a35
Bhumika_Dolnare01 <dolnarebhumika@gmail.com>
2026-03-10 22:51:51 +0530

    Delete routes/quizRoutes.js

--- routes/quizRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { generateQuiz } from "../controllers/quizController.js";
-
-const router = express.Router();
-
-router.post("/generate-quiz", generateQuiz);
-
-export default router;
