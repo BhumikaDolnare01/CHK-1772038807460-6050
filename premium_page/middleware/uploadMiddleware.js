3493147f7774bbac8c07c925beed3ccc56f48b9f
Bhumika_Dolnare01 <dolnarebhumika@gmail.com>
2026-03-10 22:04:42 +0530

    Add Middleware

---
+++ premium_page/middleware/uploadMiddleware.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage(); // store file in memory as buffer
+
+const fileFilter = (_req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF files are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB max
+  },
+});
+
+export default upload;
